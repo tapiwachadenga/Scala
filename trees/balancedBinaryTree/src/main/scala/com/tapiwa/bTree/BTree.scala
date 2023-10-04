36af1e11fe1f2db8b6d8fff3b1d1c2da0edcdcb5
Tapiwa Chadenga <tapiwachadenga@gmail.com>
2023-10-04 16:08:34 -0500

    changed BTree.Leaf to object and extended to Nothing

--- trees/balancedBinaryTree/src/main/scala/com/tapiwa/bTree/BTree.scala
+++ trees/balancedBinaryTree/src/main/scala/com/tapiwa/bTree/BTree.scala
@@ -1,6 +1,6 @@
 package com.tapiwa.bTree
 
 trait BTree[T]  
-case class Leaf[T]() extends BTree[T]
+case object Leaf extends BTree[Nothing]
 case class Branch[T](value: T, left: BTree[T], right: BTree[T]) extends BTree[T]
 
